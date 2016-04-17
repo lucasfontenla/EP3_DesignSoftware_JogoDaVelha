83e59ac31db04efd73ac2e96f70045da697dabb7
lucasfontenla <lucascfontenla@hotmail.com>
2016-04-17 16:23:09 -0300

    Da para adicionar em dicionários. A busca vai ficar muito mais eficiente

--- teste_criar_varios_botoes.py
+++ teste_criar_varios_botoes.py
@@ -1,14 +1,18 @@
 import tkinter as tk
 
-window = tk.Tk()
-number_of_buttons = 9
-buttons = [0]*number_of_buttons
-
-print(len(buttons))
-
-for i in range(0, number_of_buttons):
-    buttons[i] = tk.Button(window)
-    buttons[i].pack()
-    print(i)
+def gera_botoes(top_level, numero_de_botoes):
+    botoes = dict()
     
+    for i in range(0, numero_de_botoes):
+        botoes["Botão{0}".format(i)] = tk.Button(top_level)
+        
+    return botoes
+
+window = tk.Tk()    
+
+botoes = gera_botoes(window, 9)
+    
+for j in botoes.values():
+    j.pack()    
+
 window.mainloop()

73be3344b758694f95d8f90a526e5bef5ca81cfe
lucasfontenla <lucascfontenla@hotmail.com>
2016-04-18 21:57:56 -0300

    Na classe tabuleiro foi adicionado as seguintes ações: Depois da jogada, o botão é desabilitado, agora mostra-se o resultado do jogo em uma janela pop up e ao mesmo tempo reseta os botões, apagando o texto e os habilitando novamente

--- tabuleiro.py
+++ tabuleiro.py
@@ -9,6 +9,7 @@ class Tabuleiro:
     def __init__(self):
         #window
         self.window = tk.Tk()
+        self.window.title("Jogo da Velha")
         self.window.geometry("450x520")
         for k in range(0, 3): #gera as linhas e colunas números 0, 1 e 2
             self.window.rowconfigure(k, minsize=150)
@@ -29,12 +30,13 @@ class Tabuleiro:
         label1 = tk.Label(self.window)
         label1.configure(width=60, height=1)
         label1.grid(row=3, column=0, columnspan=3, sticky="W")
-                
+
     def gera_botoes(self, posicao_tupla, letra):
         conteudo_botao = tk.StringVar()
         conteudo_botao.set(letra)
         global botoes
-        botoes[posicao_tupla[0]][posicao_tupla[1]].config(textvariable=conteudo_botao)
+        botoes[posicao_tupla[0]][posicao_tupla[1]].config(textvariable=conteudo_botao,\
+                state="disabled") #após escrever, o botão é desabilitado
      
     def gera_label(self, display): 
         conteudo_label = tk.StringVar()
@@ -44,11 +46,23 @@ class Tabuleiro:
     
     def command_botao(self, i):
         print("Click", i)
+        tela.gera_botoes(i, "X") #testar se o botão funciona
 
-    def limpa_tabuleiro(self):
+    def mostra_vencedor_reset(self, resultado): #display dos resultados + reset dos botões
+        janela_vencedor = tk.Toplevel()
+        janela_vencedor.title("Vencedor")
+        janela_vencedor.geometry("200x200")
+        janela_vencedor.rowconfigure(0, minsize="100")
+        janela_vencedor.rowconfigure(1, minsize="100")
+        janela_vencedor.columnconfigure(0, minsize="200")
+        display_vencedor = tk.Message(janela_vencedor, text=resultado, font="Verdana 10", justify="center")
+        display_vencedor.grid(row=0, column=0, sticky="nsew")
+        botao_novo_jogo = tk.Button(janela_vencedor, text="Novo Jogo", command=janela_vencedor.destroy)
+        botao_novo_jogo.grid(row=1, column=0)
         for i in range(0,3):
             for j in range(0,3):
                 tela.gera_botoes((i, j), "")
+                botoes[i][j].config(state="normal")
         
     def iniciar(self):
         self.window.mainloop()
@@ -56,8 +70,5 @@ class Tabuleiro:
 #teste das funções        
 tela = Tabuleiro()
 tela.gera_label("Legenda Teste")
-for i in range(0,3):
-    for j in range(0,3):
-        tela.gera_botoes((i, j), (i,j))
-#tela.limpa_tabuleiro()
+tela.mostra_vencedor_reset("X")
 tela.iniciar()

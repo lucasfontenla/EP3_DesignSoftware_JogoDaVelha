79f5aee754e9f39c4c3ded05822d94913cbf3332
lucasfontenla <lucascfontenla@hotmail.com>
2016-04-19 20:37:00 -0300

    Comentários adicionados. Criada função verifica ganhador que está sendo usada apenas para teste. Falta criar a função vencedor e escrever na label do tabuleilo as próximas jogadas

--- Jogo_da_Velha.py
+++ Jogo_da_Velha.py
@@ -5,32 +5,32 @@ EP3_JogoDaVelha
 """
 #NOTAS
 #começa com X
-import numpy as np
+import numpy as np #importa numpy para gerar a matrizes
 
 class Jogo:
 	def __init__(self):
-		global contador
+		global contador #contador de número de jogadas
 		contador = 0
 
 		global tabuleiro_virtual
-		tabuleiro_virtual = np.zeros([3,3])
+		tabuleiro_virtual = np.zeros([3,3]) #gera o tabuleiro como uma matriz 3x3 de zeros
 
-		self.proxima_jogada = "X"
-		self.ganhador = -1
+		self.proxima_jogada = "X" #define a primeira jogada como "X"
+		self.ganhador = -1 #O valor -1 sempre prossegue o jogo até que haja um ganhador 
 
-	def recebe_jogada(self, posicao_jogada_tupla):
+	def recebe_jogada(self, posicao_jogada_tupla): #função que recebe a jogada do tabuleiro
 		
-		self.registra_jogada(posicao_jogada_tupla)
+		self.registra_jogada(posicao_jogada_tupla) #chama a função registra jogada para ser registrada na matriz
 
 		global contador
-		contador += 1
+		contador += 1 #adc 1 cada vez que há uma jogada
 
-		if contador % 2 == 0:
+		if contador % 2 == 0: #sempre que o númeoro for par, escreve "X"
 			self.proxima_jogada = "X"
 		else:
 			self.proxima_jogada = "O"
 
-	def registra_jogada(self, posicao_jogada_tupla):
+	def registra_jogada(self, posicao_jogada_tupla): #função responsável por registrar a jogada na matriz
 		if self.proxima_jogada == "X":
 			valor = 1
 		else:
@@ -41,10 +41,10 @@ class Jogo:
 		global contador
 
 		print(tabuleiro_virtual)
-		if contador > 4:
-			self.verifica_ganhador()
+		if contador > 4: #só começa a avaliar o resultado se a número de jogadas for maior que 4, já que ninguém ganha o jogo com menos de 4 rodadas
+			self.verifica_ganhador() #chama a função que verifica ganhador
 
-	def verifica_ganhador(self): #SOMENTE PARA TESTE DAS FUÇÕES AIDANTE DESSA
+	def verifica_ganhador(self): #SOMENTE PARA TESTE DAS FUÇÕES AIDANTE --> AINDA A CRIAR VERIFICAÇÃO FINAL DE GANHADOR
 		lista_de_zeros = list()
 		for i in tabuleiro_virtual:
 			for j in i:
@@ -60,7 +60,7 @@ class Jogo:
 		else:
 			self.ganhador = -1
 
-	def limpa_jogadas(self):
+	def limpa_jogadas(self): #Função que reseta todo o tabuleiro
 		for i in range(0,3):
 			for j in range(0,3):
 				tabuleiro_virtual[i][j] = 0

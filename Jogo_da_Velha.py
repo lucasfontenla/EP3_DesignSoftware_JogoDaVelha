ef8dad5b97e627e5a96dbc4933656df43fba487c
lucasfontenla <lucascfontenla@hotmail.com>
2016-04-19 13:18:50 -0300

    Criado arquivo jogo da velha

---
+++ Jogo_da_Velha.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Lucas Fontenla & Victor Hugo - Engenharia 1B
+EP3_JogoDaVelha
+"""
+class Jogo:
+	def __init__(self):
+		self.proxima_jogada = str()
+
+	def recebe_jogada(self, i):
+		print(i)

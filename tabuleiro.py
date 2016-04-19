1474b8c72629d6225fe12f9285e8dcfdaab966a6
lucasfontenla <lucascfontenla@hotmail.com>
2016-04-18 22:58:57 -0300

    Janela pop-up rodando 100%

--- tabuleiro.py
+++ tabuleiro.py
@@ -11,6 +11,7 @@ class Tabuleiro:
         self.window = tk.Tk()
         self.window.title("Jogo da Velha")
         self.window.geometry("450x520")
+        self.window.resizable(0,0)
         for k in range(0, 3): #gera as linhas e colunas números 0, 1 e 2
             self.window.rowconfigure(k, minsize=150)
             self.window.columnconfigure(k, minsize=150)
@@ -49,26 +50,38 @@ class Tabuleiro:
         tela.gera_botoes(i, "X") #testar se o botão funciona
 
     def mostra_vencedor_reset(self, resultado): #display dos resultados + reset dos botões
-        janela_vencedor = tk.Toplevel()
-        janela_vencedor.title("Vencedor")
-        janela_vencedor.geometry("200x200")
-        janela_vencedor.rowconfigure(0, minsize="100")
-        janela_vencedor.rowconfigure(1, minsize="100")
-        janela_vencedor.columnconfigure(0, minsize="200")
-        display_vencedor = tk.Message(janela_vencedor, text=resultado, font="Verdana 10", justify="center")
+        self.janela_vencedor = tk.Toplevel()
+        self.janela_vencedor.resizable(0,0)
+        self.janela_vencedor.title("Vencedor")
+        self.janela_vencedor.geometry("200x180")
+
+        self.janela_vencedor.rowconfigure(0, minsize="100")
+        self.janela_vencedor.rowconfigure(1, minsize="30")
+        self.janela_vencedor.rowconfigure(2, minsize="30")
+        self.janela_vencedor.columnconfigure(0, minsize="200")
+
+        display_vencedor = tk.Message(self.janela_vencedor, text=resultado, font="Verdana 10", justify="center")
         display_vencedor.grid(row=0, column=0, sticky="nsew")
-        botao_novo_jogo = tk.Button(janela_vencedor, text="Novo Jogo", command=janela_vencedor.destroy)
-        botao_novo_jogo.grid(row=1, column=0)
+
+        self.botao_novo_jogo = tk.Button(self.janela_vencedor, text="Novo Jogo", width=10, command=self.limpa_tela)
+        self.botao_novo_jogo.grid(row=1, column=0)
+
+        botao_sair = tk.Button(self.janela_vencedor, text="Sair", width=10, command=self.window.destroy)
+        botao_sair.grid(row=2, column=0)
+
+    def limpa_tela(self): 
+        self.janela_vencedor.destroy()
         for i in range(0,3):
             for j in range(0,3):
                 tela.gera_botoes((i, j), "")
-                botoes[i][j].config(state="normal")
-        
+                botoes[i][j].config(state="normal")    
+
     def iniciar(self):
         self.window.mainloop()
 
 #teste das funções        
 tela = Tabuleiro()
+tela.gera_botoes((0,1), "X")
 tela.gera_label("Legenda Teste")
 tela.mostra_vencedor_reset("X")
 tela.iniciar()
